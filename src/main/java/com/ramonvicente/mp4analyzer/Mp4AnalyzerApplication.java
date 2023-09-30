2775e4e7efa3d951dd996509c0dc0a7e5ced26de
Ramon Vicente <ramonvicentee@gmail.com>
2023-09-30 16:31:34 +0200

    first commit

---
+++ src/main/java/com/ramonvicente/mp4analyzer/Mp4AnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.ramonvicente.mp4analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp4AnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp4AnalyzerApplication.class, args);
+	}
+
+}
